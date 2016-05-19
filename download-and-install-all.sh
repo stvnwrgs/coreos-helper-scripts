fca25c97273f7a25242079ea3ab97f2a4eb3e64d
Steven Wirges <steven.wirges@stylelounge.de>
2016-05-19 14:53:51 +0200

    added download urls for gce metadata downloader and cfssl

--- download-and-install-all.sh
+++ download-and-install-all.sh
@@ -3,10 +3,10 @@
 
 mkdir -p /opt/bin
 cd /opt/bin
-wget https://raw.githubusercontent.com/stvnwrgs/coreos-helper-scripts/master/
+wget https://raw.githubusercontent.com/stvnwrgs/coreos-helper-scripts/master/gce-metadata-downloader.sh
 wget https://raw.githubusercontent.com/stvnwrgs/coreos-helper-scripts/master/wupiao.sh
 
-wget https://raw.githubusercontent.com/stvnwrgs/coreos-helper-scripts/master/prepare-cfssl.sh
+wget https://raw.githubusercontent.com/stvnwrgs/coreos-helper-scripts/master/install-cfssl.sh
 
 chmod +x gce-metadata-downloader.sh wupiao.sh prepare-cfssl.sh
 
